3ab7b0618855198d71502ee9f392d375691a11d7
Prashant Agrawal <prashant.agrawal@opumtechnologies.com>
2022-07-05 03:14:22 +1200

    add tasks component

---
+++ angular-crash/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task : Task =  {
+    text: '',
+    day: '',
+    reminder: true,
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
