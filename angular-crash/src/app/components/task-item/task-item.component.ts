a8138313c5ca55c38215e471ad83540968852848
Prashant Agrawal <prashant.agrawal@opumtechnologies.com>
2022-07-05 03:31:29 +1200

    add delete icon

--- angular-crash/src/app/components/task-item/task-item.component.ts
+++ angular-crash/src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -13,6 +15,8 @@ export class TaskItemComponent implements OnInit {
     day: '',
     reminder: true,
   };
+
+  faTimes = faTimes;
 
   constructor() { }
 
