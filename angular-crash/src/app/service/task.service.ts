05417b5df14c12b124dc549f2b0a1c45dcd040cb
Prashant Agrawal <prashant.agrawal@opumtechnologies.com>
2022-07-06 03:41:39 +1200

    able to add tasks

--- angular-crash/src/app/service/task.service.ts
+++ angular-crash/src/app/service/task.service.ts
@@ -4,8 +4,8 @@ import { Task } from '../Task';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 const httpOptions = {
-  headers : new HttpHeaders({
-    'Content-Type' : "application/json"
+  headers: new HttpHeaders({
+    'Content-Type': "application/json"
   })
 }
 
@@ -16,16 +16,20 @@ export class TaskService {
   private apiUrl: string = "http://localhost:5000/tasks";
   constructor(private client: HttpClient) { }
 
+  addTask(task: Task): Observable<Task> {
+    return this.client.post<Task>(this.apiUrl, task, httpOptions)
+  }
+
   getTasks(): Observable<Task[]> {
     return this.client.get<Task[]>(this.apiUrl)
   }
 
-  deteteTask(task:Task): Observable<Task> {
+  deteteTask(task: Task): Observable<Task> {
     const url: string = `${this.apiUrl}/${task.id}`;
     return this.client.delete<Task>(url)
   }
 
-  updateTask(task:Task): Observable<Task> {
+  updateTask(task: Task): Observable<Task> {
     const url: string = `${this.apiUrl}/${task.id}`;
     return this.client.put<Task>(url, task, httpOptions)
   }
